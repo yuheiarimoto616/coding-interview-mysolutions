a64590b65e3fe2aa0d4121aab6ec96cbbfeadbde
yuheiarimoto616 <yuhei61627@icloud.com>
2024-10-10 01:46:17 -0700

    retried 27. Remove Element

--- remove-element.js
+++ remove-element.js
@@ -1,14 +1,27 @@
 // 27. Remove Element (Easy) from LeetCode
 
 function removeElement(nums, val) {
-    let count = 0;
-
+    let i = 0;
     for (let num of nums) {
         if (num != val) {
-            nums[count] = num;
-            count++;
+            nums[i] = num;
+            i++;
         }
     }
 
-    return count;
+    return i;
 }
+
+
+// function removeElement(nums, val) {
+//     let count = 0;
+
+//     for (let num of nums) {
+//         if (num != val) {
+//             nums[count] = num;
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
