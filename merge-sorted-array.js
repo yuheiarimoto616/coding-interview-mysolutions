5e988f332f2910a3d9011a5badb8dc96bc4c1315
yuheiarimoto616 <yuhei61627@icloud.com>
2024-10-10 01:38:47 -0700

    merge-sorted-array question retried and updated

--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -5,22 +5,38 @@ function merge(nums1, m, nums2, n) {
         return;
     }
 
-    let index = m + n - 1;
+    let i = m + n - 1;
 
-    m--; 
-    n--;
-    
-    while (n >= 0) {
-        if (nums1[m] >= nums2[n]) {
-            nums1[index] = nums1[m];
-            m--;
-        }  else {
-            nums1[index] = nums2[n];
-            n--;
-        }
-        index--;
+    let a = m - 1;
+    let b = n - 1;
+
+    while (b >= 0) {
+        nums1[i] = (a >= 0 && (nums1[a] > nums2[b])) ?  nums1[a--] : nums2[b--];
+        i--;
     }
 }
+
+// function merge(nums1, m, nums2, n) {
+//     if (n == 0) {
+//         return;
+//     }
+
+//     let index = m + n - 1;
+
+//     m--; 
+//     n--;
+    
+//     while (n >= 0) {
+//         if (nums1[m] >= nums2[n]) {
+//             nums1[index] = nums1[m];
+//             m--;
+//         }  else {
+//             nums1[index] = nums2[n];
+//             n--;
+//         }
+//         index--;
+//     }
+// }
 
 let nums1 = [1,2,3,0,0,0];
 let nums2 = [2,5,6];
