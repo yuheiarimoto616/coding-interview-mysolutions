b81ead18e65d57739a93f47c49c0302834881de1
yuheiarimoto616 <yuhei61627@icloud.com>
2024-10-11 06:01:39 -0700

    80. Remove duplicates from sorted array II (Medium) from LeetCode

--- remove-duplicates-sorted-array2.js
+++ remove-duplicates-sorted-array2.js
@@ -1,4 +1,28 @@
 // 80. Remove duplicates from sorted array II (Medium) from LeetCode
 function removeDuplicates(nums) {
-    
+    let count = 1;
+    let repeatCount = 1;
+    let currentNum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (currentNum != nums[i]) {
+            currentNum = nums[i];
+            nums[count] = nums[i];
+            count++;
+            repeatCount = 1;
+        } else if (repeatCount < 2) {
+            nums[count] = nums[i];
+            count++;
+            repeatCount++;
+        }
+    }
+
+    return count;
 }
+
+let nums = [0,0,1,1,1,1,2,3,3];
+
+let k = removeDuplicates(nums);
+
+console.log('k :>> ', k);
+console.log('nums :>> ', nums);
