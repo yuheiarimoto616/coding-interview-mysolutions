8451c84d5eb34c00728efd190f40d676fe564717
yuheiarimoto616 <yuhei61627@icloud.com>
2024-10-13 06:11:43 -0700

    Refined solution for 122. Best Time to Buy and Sell Stock II (Medium) LeetCode

--- stock-medium.js
+++ stock-medium.js
@@ -1,32 +1,46 @@
 // 122. Best Time to Buy and Sell Stock II (Medium) LeetCode
 function maxProfit(prices) {
-    let prevBuyPrice = prices[0];
-    let buyPrice  = prices[0];
     let profit = 0;
-    let prevProfit = 0;
-
+    let prevBuyPrice = prices[0];
     for (let price of prices) {
-        if (buyPrice > price) {
-            buyPrice = price;
-            continue;
+        if (price > prevBuyPrice) {
+            profit += price - prevBuyPrice;
         }
 
-        if ((profit - prevProfit) + (price - prevBuyPrice) > (profit + (price - buyPrice))) {
-            profit = (profit - prevProfit) + (price - prevBuyPrice);
-            prevProfit = (price - prevBuyPrice);
-        } else {
-            profit += (price - buyPrice);
-            prevProfit = (price - buyPrice);
-            prevBuyPrice = buyPrice;
-        }
-
-        buyPrice = price;
+        prevBuyPrice = price;
     }
 
     return profit;
 }
 
-let prices = [7,6,4,3,1];
+// function maxProfit(prices) {
+//     let prevBuyPrice = prices[0];
+//     let buyPrice  = prices[0];
+//     let profit = 0;
+//     let prevProfit = 0;
+
+//     for (let price of prices) {
+//         if (buyPrice > price) {
+//             buyPrice = price;
+//             continue;
+//         }
+
+//         if ((profit - prevProfit) + (price - prevBuyPrice) > (profit + (price - buyPrice))) {
+//             profit = (profit - prevProfit) + (price - prevBuyPrice);
+//             prevProfit = (price - prevBuyPrice);
+//         } else {
+//             profit += (price - buyPrice);
+//             prevProfit = (price - buyPrice);
+//             prevBuyPrice = buyPrice;
+//         }
+
+//         buyPrice = price;
+//     }
+
+//     return profit;
+// }
+
+let prices = [7,1,5,3,8,6,7];
 
 let ret = maxProfit(prices);
 
