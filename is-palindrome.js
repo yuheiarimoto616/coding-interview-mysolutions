32b29e10acab87d40c95d0e19116ee8d20627eec
yuheiarimoto616 <yuhei61627@icloud.com>
2024-11-09 05:25:42 -0800

    125. Valid Palindrome (Easy) from LeetCode

--- is-palindrome.js
+++ is-palindrome.js
@@ -1,4 +1,27 @@
 // 125. Valid Palindrome (Easy) from LeetCode https://leetcode.com/problems/valid-palindrome/  
 function isPalindrome(s) {
-    
+    s = s.toLowerCase().match(/[a-z0-9]/g);
+
+    if (s == null) {
+        return true;
+    }
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
 }
+
+let string = ""
+
+let ret = isPalindrome(string);
+
+console.log('ret :>> ', ret);
